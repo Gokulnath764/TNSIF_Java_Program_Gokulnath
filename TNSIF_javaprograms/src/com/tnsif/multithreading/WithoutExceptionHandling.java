0f0114ca93a5c5c0197f30f696b0f83f8555b844
JET1 <JET1@DESKTOP-9HPMHBK>
2025-07-05 20:26:28 +0530

    Initial Commit

---
+++ TNSIF_javaprograms/src/com/tnsif/multithreading/WithoutExceptionHandling.java
@@ -0,0 +1,10 @@
+package com.tnsif.multithreading;
+
+public class WithoutExceptionHandling {
+
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+	
+}
